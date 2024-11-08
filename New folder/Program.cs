1a5ec4e2d6c9f84a7134450ec8796fd751a7de67
Apark <apaksara.bupa@bumail.net>
2024-11-09 00:14:27 +0700

    Add files via upload

---
+++ New folder/Program.cs
@@ -0,0 +1,10 @@
+﻿using StudentGrades;
+
+var gradeCalculator = new GradeCalculator();
+
+Console.Write("Enter the percentage : ");
+var percentage = Convert.ToInt32(Console.ReadLine());
+
+var grade = gradeCalculator.GetGradePercentage(percentage);
+
+Console.WriteLine($"Student Grade : {grade}");
